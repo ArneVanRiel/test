8e69f67c1f03a36a28994eca9d9aedc19222cc3f
ArneVanRiel <84543144+ArneVanRiel@users.noreply.github.com>
2023-01-31 22:53:52 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,25 +1,7 @@
 from tkinter import *
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import streamlit as st
 
-def plot():
-    fig = Figure(figsize = (5, 5), dpi = 100)        
-    y = [i**2 for i in range(101)]
-    plot1 = fig.add_subplot(111)
-    plot1.plot(y)
-
-    
-    canvas = FigureCanvasTkAgg(fig,master = window)
-    canvas.draw()
-    canvas.get_tk_widget().pack()
-    toolbar = NavigationToolbar2Tk(canvas,window)
-    toolbar.update()
-    canvas.get_tk_widget().pack()
-    
-window = Tk()
-window.title('Plotting in Tkinter')
-window.state('zoomed')   #zooms the screen to maxm whenever executed
-
-plot_button = Button(master = window,command = plot, height = 2, width = 10, text = "Plot")
-plot_button.pack()
-window.mainloop()
+st.set_page_config(layout='wide')
+st.title("test")
