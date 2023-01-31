115c80e9e83803515d7bfc28e987e8142c254cc6
ArneVanRiel <84543144+ArneVanRiel@users.noreply.github.com>
2023-02-01 00:16:01 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+import pandas as pd
 import streamlit as st
+import glob
 
-st.set_page_config(layout='wide')
+
+imput_analyse_bedrijven = st.sidebar.text_input("vul hier een map in waar excelbestanden instaan :", "C:/Users/info/OneDrive/Documenten/ARNE/program/excel_calc/")
+excel_files = glob.glob(str(imput_analyse_bedrijven) + "*.xlsx")
+
+aandelen_calc_list = [pd.read_excel(filename) for filename in excel_files]
+
+
 st.title("test")
-st.text("Ready")
+if(st.button('tabel met bestanden')):
+    st.text("Ready")
+
+    st.dataframe(aandelen_calc_list)
+
